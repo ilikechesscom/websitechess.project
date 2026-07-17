a7ecc6360dee391cf4c17e7f3352b21dff5b68e1
ilikechesscom <watering0624@gmail.com>
2026-07-17 15:16:38 +0900

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const board = document.getElementById("board");
 
 const pieces = [
-    ["♜","♞","♝","♛","♚","♝","♞","♜"],
-    ["♟","♟","♟","♟","♟","♟","♟","♟"],
+    ["black-rook.png","black-knight.png","black-bishop.png","black-queen.png","black-king.png","black-bishop.png","black-knight.png","black-rook.png"],
+    ["black-pawn.png","black-pawn.png","black-pawn.png","black-pawn.png","black-pawn.png","black-pawn.png","black-pawn.png","black-pawn.png"],
     ["","","","","","","",""],
     ["","","","","","","",""],
     ["","","","","","","",""],
     ["","","","","","","",""],
-    ["♙","♙","♙","♙","♙","♙","♙","♙"],
-    ["♖","♘","♗","♕","♔","♗","♘","♖"]
+    ["white-pawn.png","white-pawn.png","white-pawn.png","white-pawn.png","white-pawn.png","white-pawn.png","white-pawn.png","white-pawn.png"],
+    ["white-rook.png","white-knight.png","white-bishop.png","white-queen.png","white-king.png","white-bishop.png","white-knight.png","white-rook.png"]
 ];
 
 for (let row = 0; row < 8; row++) {
@@ -16,14 +16,15 @@ for (let row = 0; row < 8; row++) {
         const square = document.createElement("div");
 
         square.classList.add("square");
+        square.classList.add((row + col) % 2 === 0 ? "white" : "black");
 
-        if ((row + col) % 2 === 0) {
-            square.classList.add("white");
-        } else {
-            square.classList.add("black");
+        if (pieces[row][col] !== "") {
+            const img = document.createElement("img");
+            img.src = "images/" + pieces[row][col];
+            img.classList.add("piece");
+            img.alt = "";
+            square.appendChild(img);
         }
-
-        square.textContent = pieces[row][col];
 
         board.appendChild(square);
     }
