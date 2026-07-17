ab024d32da950d094bf3cdfb6359d57815b72319
ilikechesscom <watering0624@gmail.com>
2026-07-17 13:28:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-console.log("Chess Game");
-document.getElementById("board").innerHTML = "♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜";
+const board = document.getElementById("board");
+
+
+const pieces = [
+    "♜","♞","♝","♛","♚","♝","♞","♜",
+    "♟","♟","♟","♟","♟","♟","♟","♟",
+    "","","","","","","","",
+    "","","","","","","","",
+    "","","","","","","","",
+    "","","","","","","",
+    "♙","♙","♙","♙","♙","♙","♙","♙",
+    "♖","♘","♗","♕","♔","♗","♘","♖"
+];
+
+
+for(let i = 0; i < 64; i++){
+
+    let square = document.createElement("div");
+
+    square.classList.add("square");
+
+
+    if((Math.floor(i/8)+i)%2==0){
+        square.classList.add("white");
+    }
+    else{
+        square.classList.add("black");
+    }
+
+
+    square.innerHTML = pieces[i];
+
+
+    board.appendChild(square);
+}
